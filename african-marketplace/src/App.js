276ef7d6631e5d603117d65e989b0e8691f669ba
unknown <Katherine2bk@gmail.com>
2020-11-12 13:27:28 -0800

    imported switch and route inside app.js, added notes to remember... set up utils folder with axiosWithAuth as well as images folder, and components folder... building components now

--- african-marketplace/src/App.js
+++ african-marketplace/src/App.js
@@ -1,8 +1,15 @@
 import './App.css';
+import {Switch, Route} from "react-router-dom";
+//import login/register component, regular Route - Liz
+// private routes for home (dashboard), and items and add item form
 
 function App() {
   return (
     <div className="App">
+    <Switch>
+    <Route exact path="/" component={} />
+    <PrivateRoute path="/home" component={} />
+    </Switch>
     </div>
   );
 }
