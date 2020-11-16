f7b3b75cc62ddf11530a85019e21d31c7cde26d9
unknown <Katherine2bk@gmail.com>
2020-11-16 13:33:56 -0800

    set up files for shoppingCart action and reducer, added shopping cart icon in ItemCard.js

--- african-marketplace/src/Cards/ItemCard.js
+++ african-marketplace/src/Cards/ItemCard.js
@@ -1,4 +1,6 @@
 import React, { useState, useEffect } from "react";
+import {AiFillPlusCircle} from "react-icons/ai";
+import {IconContext} from "react-icons"
 import {
   ProductCard,
   Title,
@@ -12,13 +14,14 @@ import {
 import { CardText, CardBody, Collapse } from "reactstrap";
 
 const ItemCard = (props) => {
-    const [isOpen, setIsOpen] = useState(false);
-    const toggle = () => setIsOpen(!isOpen);
 
     //need data from product endpoints: title, username
     // do we need imgs?
 
+    //map through items...
+
     return (
+        <IconContext.Provider value={{ style: {fontSize: '40px', color: 'yellow', cursor: 'pointer'}}}>
         <ProductCard>
         <Title>Name of product</Title>
         <CardOverlay>
@@ -27,8 +30,12 @@ const ItemCard = (props) => {
         <CardBody>
           <SubTitle>Description: data description</SubTitle>
         </CardBody>
-      <Price>Price: data price</Price>
-    </ProductCard>
+      <Price>Price: data price</Price> 
+        <div>
+        <span style={{color: 'orange', fontSize: '20px', fontWeight: 'bold'}}>Add to Cart <AiFillPlusCircle /> </span>
+        </div>
+      </ProductCard>
+    </IconContext.Provider>
     )
 }
 
