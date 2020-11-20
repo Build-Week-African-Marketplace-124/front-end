2850240bbed7a26d7ef1830fbb5dd073165a0583
unknown <Katherine2bk@gmail.com>
2020-11-20 14:46:33 -0800

    added search bar functionality, used connect to pass in data from reducer, set up handleChanges and an if statement to render items/products depending on search results, passing data to ItemCard

--- african-marketplace/src/Cards/Items.js
+++ african-marketplace/src/Cards/Items.js
@@ -23,11 +23,90 @@ import { Wrapper, ItemWrapper,
         console.log("fetched products");
     }, [])
 
+    //searchbar function
+
+    useEffect(() => {
+        const results = props.productData.filter(
+            descriptions =>
+            descriptions.name.toLowerCase().includes(search.toLowerCase()) ||
+            descriptions.category.toLowerCase().includes(search.toLowerCase())
+        );
+        setSearchResults(results)
+    }, [search, props.productData]);
+
+    const handleChanges = (event) => {
+        event.preventDefault();
+        setSearch(event.target.value);
+    }
+
+    let listRender;
+
+    if (search.length === 0) {
+        listRender = (
+            <>
+            <ItemWrapper>
+            {props.productData.map(item => (
+                <ItemCard 
+                key={item.id}
+                data={item}
+                allData={props.productData}
+                username={props.productData.username}
+                />
+            ))}
+            </ItemWrapper>
+            </>
+        );
+    } else {
+        listRender = (
+            <>
+            <ItemWrapper>
+            {searchResults.map(item => (
+                <ItemCard
+                    key={item.id}
+                    data={item}
+                    allData={props.productData}
+                    username={props.productData.username}
+                />
+            ))}
+            </ItemWrapper>
+            </>
+        );
+    }
+
+    console.log("props from Items", props)
 
     return (
-        <div>
-        
-        </div>
+        <>
+        <Wrapper>
+        <Heading>
+            <Title>MARKET PLACE</Title>
+            <SearchBar>
+                <InputGroupAddon addonType="prepend">
+                    <InputGroupText>
+                    <FaSearch />
+                    </InputGroupText>
+                </InputGroupAddon>
+
+               <SearchInput
+                    onChange={handleChanges}
+                    value={searchTerm}
+                    type="search"
+                    placeholder="Search Products"
+                    name="search"
+               />
+            </SearchBar>
+        </Heading>
+        {props.isFetching ? (
+            <div>
+            <Spinner color="danger" />
+            <br />
+            <p>Loading...</p>
+            </div>
+        ) : (
+            listRender
+        )} 
+        </Wrapper>
+        </>
     )
   }
 
